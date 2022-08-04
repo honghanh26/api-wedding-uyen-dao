09e2f59afcc7db5acb105ab62542e49735076532
Hanh Cao <caothihonghanh26@gmail.com>
2022-08-04 13:46:17 +0700

    fix type for gallery id

--- app/schemas/galleries.js
+++ app/schemas/galleries.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const databaseConfig = require(__path_configs + 'database');
 
 var schema = new mongoose.Schema({
-    id: String,
+    id: Number,
     name: String,
     description: String,
     img: Array
